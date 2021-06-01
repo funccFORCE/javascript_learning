a78b07f3822ad44183d2e4eea2e17f6726eba28f
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-06-01 14:53:12 +0530

    update fetchscript.js

--- fetchAPI/fetchscript.js
+++ fetchAPI/fetchscript.js
@@ -3,16 +3,41 @@ let byBtn=document.getElementById("myBtn");
 
 let content = document.getElementById("content");
 
+//getData in fetchAPI
 function getData(){
-    console.log("started getdata")
-    url="aditi.txt";
+    console.log("starting getdata")
+    url ="https://api.github.com/users";
     fetch(url).then((response)=>{
-        console.log("Inside first then")
-        return Response.text();
-    }).then((data)=>{
         console.log("inside first then")
-        console.log(data);
+        return response.text();
+    }).then((data)=>{
+        console.log("inside second then")
+        console.log(data)
     })
+
+
+}
+
+//postDta in fetchAPI
+
+function postData() {
+    
+    url = "https://api.github.com/users";
+    
+    params={
+        method:'post',
+        headers:{
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    fetch(url, params).then((response) => {
+        return response.text();
+    }).then((data) => {
+        console.log(data)
+    })
+
+
 }
 
 console.log("before get data")
