11d9fbfae6bf10ef5ea50b5626a2c5268cc17d7b
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-06-11 15:58:11 +0530

    Update app.js

--- notes website/app.js
+++ notes website/app.js
@@ -1,1 +1,18 @@
+
 console.log("welcome to notes app");
+let addBtn=document.getElementById('addBtn');
+addBtn.addEventListener("click",function(e){
+    let addText=document.getElementById("addText");
+    let notes=localStorage.getItem("notes"); //e is an event object
+    if(notes==null){
+        notesObj=[];
+    }
+    else{
+        notesObj=JSON.parse(notes);
+}
+notesObj.push(addText.value);
+localStorage.setItem("notes",JSON.stringify(notesObj));
+addText.value="";
+console.log(notesObj);
+
+})
